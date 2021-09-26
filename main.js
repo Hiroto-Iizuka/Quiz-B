94e17adbaf1c41bd5402a61cf22ce3f0ece5b469
Hiroto-Iizuka <oroxith@gmail.com>
2021-09-26 13:14:59 +0900

    取得したクイズデータをHTMLに一覧として表示

--- main.js
+++ main.js
@@ -3,7 +3,29 @@ const startButton = document.getElementById('startButton');
 startButton.addEventListener('click', () => {
   fetch(QUIZ_API)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+      const quizData = data.results;
+      const quizList = document.getElementById('quiz-list');
+
+      quizData.forEach((quiz, index) => {
+        const numberTitleItem = document.createElement('li');
+        numberTitleItem.textContent = `${index + 1}件目のクイズデータ`;
+        quizList.appendChild(numberTitleItem);
+
+        const quizDataList = buildQuizList(quiz);
+        numberTitleItem.appendChild(quizDataList);
+      });
+    });
+    
+    const buildQuizList = (quiz) => {
+      const quizContainer = document.createElement('ul');
+      for(const prop in quiz) {
+        const item = document.createElement('li');
+        item.innerHTML = `<storng>${prop}</storng>: ${quiz[prop]}`;
+        quizContainer.appendChild(item);
+      }
+      return quizContainer;
+    }
   });
 
 
